9ca0a319d945630b40610e01793227b75c3d1fbd
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-03 01:51:47 +0200

    - testify lib added; - all test cases in access_token_test replaced with equivalent from testify; code looks more readable;

--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -3,44 +3,27 @@ package access_token
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAccessTokenConstants(t *testing.T) {
-	if expirationTime != 24 {
-		t.Error("expiration time should be 24 hours")
-	}
+	assert.EqualValues(t, 24, expirationTime, "expiration time should be 24 hours")
 }
 
 func TestGetNewAccessToken(t *testing.T) {
 	at := GetNewAccessToken()
 
-	if at.IsExpired() {
-		t.Error("new token should not be expired")
-	}
-
-	if at.AccessToken != "" {
-		t.Error("new token should not have defined access id")
-	}
-
-	if at.UserId != 0 {
-		t.Error("new token should not have an assosiated userId")
-	}
-
-	if at.ClientId != 0 {
-		t.Error("new token should not have an assosiated clientId")
-	}
+	assert.False(t, at.IsExpired(), "new token should not be expired")
+	assert.EqualValues(t, "", at.AccessToken, "new token should not have defined access id")
+	assert.True(t, at.UserId == 0, "new token should not have an assosiated userId")
+	assert.EqualValues(t, 0, at.ClientId, "new token should not have an assosiated clientId")
 }
 
 func TestAccessTokenIsExpired(t *testing.T) {
 	at := AccessToken{}
-
-	if !at.IsExpired() {
-		t.Error("empty access token should be expired by default")
-	}
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
 
 	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
-	if at.IsExpired() {
-		t.Error("access token created three hours from now should not be expired by default")
-	}
-
+	assert.False(t, at.IsExpired(), "access token created three hours from now should not be expired by default")
 }
