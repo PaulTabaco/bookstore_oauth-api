869c56a6f07f10a827438dd0ef79018d83b1e1ef
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-04 19:11:46 +0200

    get cassandra session added into GetById repository method;

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/PaulTabaco/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/domain/access_token"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/utils/errors"
 )
@@ -16,5 +17,11 @@ func NewRepository() DbRepository {
 }
 
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
 	return nil, errors.NewInternalServerError("db connection not implemented")
 }
