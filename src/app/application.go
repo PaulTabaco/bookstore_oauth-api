2e237330215f48f81ee3312d28c37ee0885f59aa
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-04 19:09:07 +0200

    Cassandra availability check at app start added to application.go;

--- src/app/application.go
+++ src/app/application.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"fmt"
+
+	"github.com/PaulTabaco/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/domain/access_token"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/http"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/repository/db"
@@ -10,9 +13,20 @@ import (
 var router = gin.Default()
 
 func StartApplication() {
+	checkCassandraAtStart()
+
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
 
 	router.Run(":8181")
+}
+
+func checkCassandraAtStart() {
+	session, dbErr := cassandra.GetSession()
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	session.Close()
+	fmt.Println("*** Cassandra availability checked at start")
 }
