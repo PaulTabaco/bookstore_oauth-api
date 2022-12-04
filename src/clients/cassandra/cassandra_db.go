7db1c4a215908083da6b50ae57bd387e2ec2e8da
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-04 19:06:46 +0200

    - db package added with cassandra driver setup;

---
+++ src/clients/cassandra/cassandra_db.go
@@ -0,0 +1,19 @@
+package cassandra
+
+import (
+	"github.com/gocql/gocql"
+)
+
+var (
+	cluster *gocql.ClusterConfig
+)
+
+func init() {
+	cluster = gocql.NewCluster("172.17.0.3")
+	cluster.Keyspace = "oauth"
+	cluster.Consistency = gocql.One //gocql.Quorum
+}
+
+func GetSession() (*gocql.Session, error) {
+	return cluster.CreateSession()
+}
