86ece18a18a31e7640d2a8c211c51397f972e9d7
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-05 13:23:36 +0200

    - Validate method added to access_token model - CREATE and UPDATE methods added to db DbRepository; - CREATE and UPDATE methods added to Service; - CREATE ( UPDATE commented) routs added to handler (http_access_token); - CREATE route added to application;

--- src/http/http_access_token.go
+++ src/http/http_access_token.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 
 	"github.com/PaulTabaco/bookstore_oauth-api/src/domain/access_token"
+	"github.com/PaulTabaco/bookstore_oauth-api/src/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 type AccessTokenHandler interface {
 	GetById(*gin.Context)
+	Create(*gin.Context)
+	// UpdateExpirationTime(*gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -28,4 +31,19 @@ func (h *accessTokenHandler) GetById(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, accessToken)
+}
+
+func (h *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+	if err := c.ShouldBindJSON(&at); err != nil {
+		restErr := errors.NewBadRequestError("invalid JSON body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	if err := h.service.Create(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusCreated, at)
 }
--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -4,10 +4,19 @@ import (
 	"github.com/PaulTabaco/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/domain/access_token"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/utils/errors"
+	"github.com/gocql/gocql"
+)
+
+const (
+	queryGetAccessToken    = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
+	queryCreateAccessToken = "INSERT INTO access_tokens (access_token, user_id, client_id, expires) VALUES (?, ?, ?, ?);"
+	queryUpdateExpiration  = "UPDATE access_tokens SET expires=? WHERE access_token=?;)"
 )
 
 type DbRepository interface {
 	GetById(string) (*access_token.AccessToken, *errors.RestErr)
+	Create(access_token.AccessToken) *errors.RestErr
+	UpdateExpirationTime(access_token.AccessToken) *errors.RestErr
 }
 
 type dbRepository struct{}
@@ -19,9 +28,58 @@ func NewRepository() DbRepository {
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
 	session, err := cassandra.GetSession()
 	if err != nil {
-		panic(err)
+		return nil, errors.NewInternalServerError(err.Error())
 	}
 	defer session.Close()
 
-	return nil, errors.NewInternalServerError("db connection not implemented")
+	var result access_token.AccessToken
+	if err := session.Query(queryGetAccessToken, id).Scan(
+		&result.AccessToken,
+		&result.UserId,
+		&result.ClientId,
+		&result.Expires,
+	); err != nil {
+		if err == gocql.ErrNotFound {
+			return nil, errors.NewNotFoundError("no access token found with given id")
+		}
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+
+	return &result, nil
+}
+
+func (r *dbRepository) Create(at access_token.AccessToken) *errors.RestErr {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer session.Close()
+
+	if err := session.Query(queryCreateAccessToken,
+		at.AccessToken,
+		at.UserId,
+		at.ClientId,
+		at.Expires,
+	).Exec(); err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+
+	return nil
+}
+
+func (r *dbRepository) UpdateExpirationTime(at access_token.AccessToken) *errors.RestErr {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer session.Close()
+
+	if err := session.Query(queryUpdateExpiration,
+		at.Expires,
+		at.AccessToken,
+	).Exec(); err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+
+	return nil
 }
