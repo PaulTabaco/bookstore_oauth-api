7789283ab7fdefe4a17fddb0fe53a6a3f70b6fcc
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-06 18:44:10 +0200

    fix session was closed in application.go

--- src/app/application.go
+++ src/app/application.go
@@ -1,9 +1,6 @@
 package app
 
 import (
-	"fmt"
-
-	"github.com/PaulTabaco/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/domain/access_token"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/http"
 	"github.com/PaulTabaco/bookstore_oauth-api/src/repository/db"
@@ -13,21 +10,10 @@ import (
 var router = gin.Default()
 
 func StartApplication() {
-	checkCassandraAtStart()
-
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
 	router.POST("/oauth/access_token", atHandler.Create)
 
 	router.Run(":8181")
-}
-
-func checkCassandraAtStart() {
-	session, dbErr := cassandra.GetSession()
-	if dbErr != nil {
-		panic(dbErr)
-	}
-	session.Close()
-	fmt.Println("*** Cassandra availability checked at start")
 }
