d991dee8927df9f49014161778f462fd31294017
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-07 16:02:36 +0200

    - golang-restclient - go framework for mocking added; NOT WORKING in new golang vers; - user_repository_test added to test user_repository, but not working because of golang-restclient;

---
+++ src/repository/rest/user_repository_test.go
@@ -0,0 +1,119 @@
+package rest
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/mercadolibre/golang-restclient/rest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("about to start test cases ...")
+	rest.StartMockupServer()
+	os.Exit(m.Run())
+}
+
+func TestLoginUserTimeoutFromApi(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		HTTPMethod:   http.MethodPost,
+		URL:          "http://localhost:8080/users/login",
+		ReqBody:      `{"email": "some@email.com", "password": "abc123"}`,
+		RespHTTPCode: -1,
+		RespBody:     `{}`,
+	})
+
+	repository := usersRepository{}
+
+	user, err := repository.LoginUser("some@email.com", "123")
+
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "invalid restclient response when triyng to login user", err.Message)
+}
+
+func TestLoginUserInvalidErrorInterface(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		HTTPMethod:   http.MethodPost,
+		URL:          "https://api.bookstore.com/users/login",
+		ReqBody:      `{"email":"email@gmail.com","password":"the-password"}`,
+		RespHTTPCode: http.StatusNotFound,
+		RespBody:     `{"message": "invalid login credentials", "status": "404", "error": "not_found"}`,
+	})
+
+	repository := usersRepository{}
+
+	user, err := repository.LoginUser("email@gmail.com", "the-password")
+
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "invalid error interface when trying to login user", err.Message)
+}
+
+func TestLoginUserInvalidLoginCredentials(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		HTTPMethod:   http.MethodPost,
+		URL:          "https://api.bookstore.com/users/login",
+		ReqBody:      `{"email":"email@gmail.com","password":"the-password"}`,
+		RespHTTPCode: http.StatusNotFound,
+		RespBody:     `{"message": "invalid login credentials", "status": 404, "error": "not_found"}`,
+	})
+
+	repository := usersRepository{}
+
+	user, err := repository.LoginUser("email@gmail.com", "the-password")
+
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.Status)
+	assert.EqualValues(t, "invalid login credentials", err.Message)
+}
+
+func TestLoginUserInvalidUserJsonResponse(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		HTTPMethod:   http.MethodPost,
+		URL:          "https://api.bookstore.com/users/login",
+		ReqBody:      `{"email":"email@gmail.com","password":"the-password"}`,
+		RespHTTPCode: http.StatusOK,
+		RespBody:     `{"id": "1", "first_name": "Fede", "last_name": "León", "email": "fedeleon.cba@gmail.com"}`,
+	})
+
+	repository := usersRepository{}
+
+	user, err := repository.LoginUser("email@gmail.com", "the-password")
+
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "error when trying to unmarshal users login response", err.Message)
+}
+
+func TestLoginUserNoError(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		HTTPMethod:   http.MethodPost,
+		URL:          "https://api.bookstore.com/users/login",
+		ReqBody:      `{"email":"email@gmail.com","password":"the-password"}`,
+		RespHTTPCode: http.StatusOK,
+		RespBody:     `{"id": 1, "first_name": "Fede", "last_name": "León", "email": "fedeleon.cba@gmail.com"}`,
+	})
+
+	repository := usersRepository{}
+
+	user, err := repository.LoginUser("email@gmail.com", "the-password")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 1, user.Id)
+	assert.EqualValues(t, "Fede", user.FirstName)
+	assert.EqualValues(t, "León", user.LastName)
+	assert.EqualValues(t, "fedeleon.cba@gmail.com", user.Email)
+}
