e04f52f30b605ef8724d87b12d1c7a929705af93
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-10 14:44:01 +0200

    bookstore_oauth-api listen port changed to 8080 (from 8181); port for accessing users api changed to 8081;

--- src/app/application.go
+++ src/app/application.go
@@ -16,5 +16,5 @@ func StartApplication() {
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
 	router.POST("/oauth/access_token", atHandler.Create)
 
-	router.Run(":8181")
+	router.Run(":8080")
 }
